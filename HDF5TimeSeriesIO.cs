a4a81a226e4ca37a6e72c1c4ad6c6865eb3572ec
Joel Rahman <joel@flowmatters.com.au>
2017-07-17 12:46:45 +1000

    Basic TimeSeries IO for exporting and importing simulation runs

--- HDF5TimeSeriesIO.cs
+++ HDF5TimeSeriesIO.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TIME.Core;
 using TIME.DataTypes;
 using TIME.DataTypes.IO;
 
 namespace FlowMatters.Source.HDF5IO
 {
-    class HDF5TimeSeriesIO : MultiTimeSeriesIO
+    public class HDF5TimeSeriesIO : MultiTimeSeriesIO
     {
-        public override string Description => "HDF5 Based data storages";
+        public override string Description => "HDF5 Based time series storage";
 
         public override string Filter => ".h5";
 
@@ -23,14 +24,88 @@ namespace FlowMatters.Source.HDF5IO
         public override void Load(FileReader reader)
         {
             var f = new HDF5File(reader.FileName);
+            data.AddRange(f.Groups.Select(kvp => ReadTimeSeries(kvp.Key, kvp.Value)).ToArray());
+        }
 
-            throw new NotImplementedException();
+        private TimeSeries ReadTimeSeries(string name, HDF5Group group)
+        {
+            double[] values = (double[]) group.DataSets["Values"].Get();
+            DateTime[] dates = ((long[]) group.DataSets["Dates"].Get()).Select(ticks => new DateTime(ticks)).ToArray();
+            var result = TimeSeriesExtensions.FromDates(dates, values);
+            result.name = RestoreName(name);
+            if (group.Attributes.Contains("Units"))
+            {
+                string unitString = (string)group.Attributes["Units"];
+                result.units = Unit.parse(unitString);
+            }
+            return result;
         }
 
         public override void Save(FileWriter writer)
         {
-//            var f = HDF.PInvoke.H5F.open
-            throw new NotImplementedException();
+            HDF5File dest = new HDF5File(writer.FileName,HDF5FileMode.WriteNew);
+            foreach (TimeSeries ts in DataSets)
+            {
+                WriteTimeSeries(dest,ts);
+            }
+
+            dest.Close();
         }
+
+        protected void WriteTimeSeries(HDF5File dest, TimeSeries ts, string path=null)
+        {
+            path = UniquePath(dest, ts, path);
+            path = H5SafeName(path);
+            dest.CreateGroup(path);
+            var grp = dest.Groups[path];
+            grp.CreateDataset("Values",ts.ToArray());
+            grp.CreateDataset("Dates",ts.Ticks());
+            grp.Attributes.Create("Units",ts.units.ToString());
+        }
+
+        private string H5SafeName(string path)
+        {
+            return path.Replace("/", "%SLASH%");
+        }
+
+        private string RestoreName(string name)
+        {
+            return name.Replace("%SLASH%", "/");
+        }
+
+        private static string UniquePath(HDF5File dest, TimeSeries ts, string path)
+        {
+            if (path == null)
+            {
+                path = ts.name;
+            }
+
+            string origPath = path;
+            int suffix = 1;
+            while (dest.Groups.ContainsKey(path) || dest.DataSets.ContainsKey(path))
+            {
+                path = $"{origPath} {suffix}";
+                suffix++;
+            }
+            return path;
+        }
+    }
+
+    public static class TimeSeriesExtensions
+    {
+        public static DateTime[] Dates(this TimeSeries ts)
+        {
+            return Enumerable.Range(0,ts.Count).Select(i => ts.timeForItem(i)).ToArray();
+        }
+
+        public static long[] Ticks(this TimeSeries ts)
+        {
+            return ts.Dates().Select(d => d.Ticks).ToArray();
+        }
+
+        public static TimeSeries FromDates(DateTime[] dates, double[] values)
+        {
+            return new TimeSeries(dates[0],TimeStep.FromSeconds((dates[1]-dates[0]).TotalSeconds),values);
+       }
     }
 }
---
+++ Tests/TestHDF5TimeSeriesIO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TIME.Core;
+using TIME.DataTypes;
+
+namespace FlowMatters.Source.HDF5IO.Tests
+{
+    [TestFixture]
+    public class TestHDF5TimeSeriesIO
+    {
+        [Test]
+        public void SaveAndLoadDaily()
+        {
+            double[] vals = Enumerable.Range(0, 1000).Select(i => (double) i).ToArray();
+            TimeSeries ts = new TimeSeries(new DateTime(1990, 1, 1), TimeStep.Daily, vals);
+            ts.name = "My Time Series";
+            ts.units = Unit.PredefinedUnit(CommonUnits.cubicMetresPerSecond);
+
+            vals = Enumerable.Range(1000, 1000).Select(i => (double) i).ToArray();
+            TimeSeries ts2 = new TimeSeries(new DateTime(1990, 1, 1), TimeStep.Daily, vals);
+            ts2.name = "Mine / Someone's Time Series";
+            ts2.units = Unit.PredefinedUnit(CommonUnits.squareMetres);
+
+            var writer = new HDF5TimeSeriesIO();
+            writer.Use(ts);
+            writer.Use(ts2);
+
+            var fn = "_TestTimeSeriesIO.h5";
+            writer.Save(fn);
+
+            var reader = new HDF5TimeSeriesIO();
+            reader.Load(fn);
+
+            TimeSeries retrieved = (TimeSeries) reader.DataSets.First(t=>t.name=="My Time Series");
+            Assert.IsTrue(ts.EqualData(retrieved));
+            Assert.AreEqual(ts.name, retrieved.name);
+            Assert.AreEqual(Unit.PredefinedUnit(CommonUnits.cubicMetresPerSecond), retrieved.units);
+
+            TimeSeries retrieved2 = (TimeSeries) reader.DataSets.First(t => t.name == "Mine / Someone's Time Series");
+            Assert.IsTrue(ts2.EqualData(retrieved2));
+            Assert.AreEqual(ts2.name, retrieved2.name);
+            Assert.AreEqual(Unit.PredefinedUnit(CommonUnits.squareMetres), retrieved2.units);
+        }
+    }
+}
