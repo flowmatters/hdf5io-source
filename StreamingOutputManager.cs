c4598efb813ff8578c0231da6cc05199b4ac0c03
Joel Rahman <joel@flowmatters.com.au>
2018-05-10 13:26:42 +1000

    Options for dealling with existing output files

--- StreamingOutputManager.cs
+++ StreamingOutputManager.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FlowMatters.H5SS;
 using RiverSystem;
 using TIME.Core;
 using TIME.DataTypes;
+using TIME.ManagedExtensions;
 using TIME.ScenarioManagement;
 using TIME.ScenarioManagement.RunManagement;
 using RiverSystemConfiguration = RiverSystem.RiverSystemConfiguration;
 
 namespace FlowMatters.Source.HDF5IO
 {
+    public enum StreamingOutputOverwriteOption
+    {
+        Fail,
+        Overwrite,
+        Increment
+    }
+
     public class StreamingOutputManager :
         IPluginInitialise<RiverSystemScenario>,
         IPluginRunStart<RiverSystemScenario>,
@@ -22,12 +29,14 @@ namespace FlowMatters.Source.HDF5IO
         public StreamingOutputManager(RiverSystemScenario scenario) 
         {
             Scenario = scenario;
+            OverwriteOption = StreamingOutputOverwriteOption.Fail;
 //            Configuration = config;
         }
 
         public RiverSystemConfiguration Configuration { get; set; }
         public RiverSystemScenario Scenario { get; set; }
         public string Destination { get; set; }
+        public StreamingOutputOverwriteOption OverwriteOption { get; set; }
         private HDF5File _destFile;
         private UniqueNameResolver _nameResolver;
         private List<HDF5TimeSeriesState> states;
@@ -80,9 +89,53 @@ namespace FlowMatters.Source.HDF5IO
             states = new List<HDF5TimeSeriesState>();
             allSeries = new List<TimeSeries>();
 
-            _destFile = new HDF5File(Destination, HDF5FileMode.WriteNew);
+            _destFile = CreateDestinationFile();
             _nameResolver = new UniqueNameResolver();
             scenario.RunManager.CurrentConfiguration.MakeResultTimeSeries = MakeStreamingTimeSeries;
+        }
+
+        private HDF5File CreateDestinationFile()
+        {
+            string fn = Destination;
+            if (File.Exists(fn))
+            {
+                switch (OverwriteOption)
+                {
+                    case StreamingOutputOverwriteOption.Fail:
+                        throw new IOException("Cannot initialise streaming. File exists");
+                    case StreamingOutputOverwriteOption.Increment:
+                        fn = IncrementFilename();
+                        break;
+                }
+            }
+
+            try
+            {
+                return new HDF5File(fn, HDF5FileMode.WriteNew);
+            }
+            catch (IOException)
+            {
+                // See if its related to the file still being 'open' and waiting closure in a finalizer
+                // Not pretty, but we don't know when Source is done with the time series...
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                return new HDF5File(fn, HDF5FileMode.WriteNew);
+            }
+        }
+
+        private string IncrementFilename()
+        {
+            string fn = Destination;
+            string ext = Path.GetExtension(Destination);
+            string baseName = Path.GetFileNameWithoutExtension(Destination); 
+            int n = 0;
+
+            while (File.Exists(fn))
+            {
+                n++;
+                fn = string.Format("{0} ({1}){2}", baseName, n, ext);
+            }
+            return fn;
         }
 
         public void ScenarioRunStart(RiverSystemScenario scenario)
@@ -101,6 +154,10 @@ namespace FlowMatters.Source.HDF5IO
 
             states.ForEach(s=>s.WriteBuffer());
             allSeries.ForEach(ts=>HDF5TimeSeriesMetadata.WriteMetadata(ts,((HDF5TimeSeriesState)ts.state).DataSet));
+
+            if (_destFile == null)
+                return;
+
             _destFile.Close();
 
             var reopened = new HDF5File(Destination,HDF5FileMode.ReadOnly);
